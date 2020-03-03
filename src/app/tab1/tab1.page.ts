afeca85e4a5f13a63146a9a442071e4a9fe55358
ismith1 <ismith1@go.olemiss.edu>
2020-03-03 15:42:06 -0600

    video

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { YoutubeVideoPlayer} from '@ionic-native/youtube-video-player/ngx';
+import { IonInfiniteScroll } from '@ionic/angular';
+
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
+})
+
+@Component({
+  selector: 'infinite-scroll-example',
+  templateUrl: 'infinite-scroll-example.html',
+  styleUrls: ['./infinite-scroll-example.css']
 })
 
 export class Tab1Page {
@@ -11,6 +20,7 @@ export class Tab1Page {
   constructor(
     private youtube: YoutubeVideoPlayer
   ) {}
+  
 
   openMyVideo(id){
     this.youtube.openVideo(id);
