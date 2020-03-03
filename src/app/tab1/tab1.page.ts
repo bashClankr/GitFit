7d098f621629203d15da5160784979ed7a828bef
Austin Marotti <austin@hst000147582um.wifi02.olemiss.edu>
2020-03-03 12:15:31 -0600

    messed with card expansion and home page

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { YoutubeVideoPlayer} from '@ionic-native/youtube-video-player/ngx';
+import { R3TargetBinder } from '@angular/compiler';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -8,12 +9,23 @@ import { YoutubeVideoPlayer} from '@ionic-native/youtube-video-player/ngx';
 
 export class Tab1Page {
 
+  item =false;
   constructor(
     private youtube: YoutubeVideoPlayer
   ) {}
 
   openMyVideo(id){
     this.youtube.openVideo(id);
+  }
+
+  expandItem(){
+    
+    if(this.item ==false){
+      this.item=true
+    }
+    else{
+      this.item=false;
+    }
   }
 }
 
