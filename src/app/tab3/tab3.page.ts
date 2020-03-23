bb222557a69eda17bd872a91815abfaf7479cddf
IsabellaSmith22 <55262521+IsabellaSmith22@users.noreply.github.com>
2020-03-23 12:15:27 -0500

    aboutAlert/Alert with information about work done

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -15,4 +15,7 @@ export class Tab3Page {
   editAlert(){
     alert("Will allow user to change their name and location on the account page");
   }
+  aboutAlert(){
+    alert("Austin: \n*Made expandable cards\n*Detailed workouts\n*Alerts on functionality\n*Homepage layout\n*Hosted on firebase\n \nIsabella: \n*Made array of card/picures to display\n*Tab design/information\n*Account page\n*Homepage layout")
+  }
 }
