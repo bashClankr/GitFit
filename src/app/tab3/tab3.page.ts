68ead15da4d9609214cae9ddbb86b3b353de9cfa
IsabellaSmith22 <55262521+IsabellaSmith22@users.noreply.github.com>
2020-03-23 12:24:00 -0500

    edited about wording

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -16,6 +16,6 @@ export class Tab3Page {
     alert("Will allow user to change their name and location on the account page");
   }
   aboutAlert(){
-    alert("Austin: \n*Made expandable cards\n*Detailed workouts\n*Alerts on functionality\n*Homepage layout\n*Hosted on firebase\n \nIsabella: \n*Made array of card/picures to display\n*Tab design/information\n*Account page\n*Homepage layout")
+    alert("Austin: \n*Front page card functionality\n*Detailed workouts\n*Alerts and buttons for future functionality\n*Homepage layout\n*Hosted on firebase\n \nIsabella: \n*Made array of card/picures to display\n*Tab design/information\n*Account page\n*Homepage layout")
   }
 }
